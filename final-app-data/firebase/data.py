1586f9235c4f713ee17e3d1bd44fabccc3838c2a
Seth Howell <54952081+sethbam9@users.noreply.github.com>
2022-10-17 15:18:07 -0400

    Update data.py

--- final-app-data/firebase/data.py
+++ final-app-data/firebase/data.py
@@ -1,10 +1,10 @@
 import firebase_admin
 from firebase_admin import credentials, db
 
-f = 'macula/tanakh-reader-firebase-adminsdk-gati8-1a91f69256.json'
+f = 'firebase-credentials.json'
 cred = credentials.Certificate(f)
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://tanakh-reader-default-rtdb.firebaseio.com/'
+    'databaseURL': ''
 })
 
 ref = db.reference(path='/')
